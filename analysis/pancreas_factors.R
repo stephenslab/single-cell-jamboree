e9fc660b67f1a30c0111089f2ba303a5f4ebe578
Peter Carbonetto <peter.carbonetto@gmail.com>
2024-11-26 14:12:40 -0600

    Added structure plots for nnlm results to pancreas_factors analysis.

--- analysis/pancreas_factors.R
+++ analysis/pancreas_factors.R
@@ -1,7 +1,3 @@
-# scale.cols(A,b) scales each column A[,i] by b[i].
-scale.cols <- function (A, b)
-  t(t(A) * b)
-
 # fastTopics.
 cells <- subsample(sample_info$celltype,n = 500)
 L <- poisson2multinom(pnmf)$L
@@ -12,27 +8,12 @@ p1 <- structure_plot(L[,batch_topics],grouping = sample_info[,"tech"],
                      gap = 10,perplexity = 70)
 p2 <- structure_plot(L[cells,celltype_topics],
                      grouping = sample_info[cells,"celltype"],
-                     gap = 20,perplexity = 70,n = Inf)
+                     gap = 25,perplexity = 70,n = Inf)
 p3 <- structure_plot(L[cells,celltype_topics2],
                      grouping = sample_info[cells,"celltype"],
-                     gap = 20,perplexity = 70,n = Inf)
+                     gap = 25,perplexity = 70,n = Inf)
 plot_grid(p1,p2,p3,nrow = 3,ncol = 1)
 
-W <- nmf$W
-k <- ncol(W)
-d <- apply(W,2,max)
-W <- scale.cols(W,1/d)
-colnames(W) <- paste0("k",1:k)
-batch_topics <- c(1,6,7,10,13,14,16,17)
-p1 <- structure_plot(W[,batch_topics],grouping = sample_info$tech,gap = 10,
-                     perplexity = 70)
-celltype_topics <- c(2,5,8,9,11,12,15,20,21,22,23)
-p2 <- structure_plot(W[cells,celltype_topics],
-                     grouping = sample_info[cells,"celltype"],
-                     gap = 20,n = Inf,perplexity = 70)
-plot_grid(p1,p2,nrow = 2,ncol = 1)
-
-# NMF.
 W <- nmf$W
 k <- ncol(W)
 d <- apply(W,2,max)
