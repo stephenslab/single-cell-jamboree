2165bdcf4115ca4c8204ec88ded7cd42cca52062
Peter Carbonetto <peter.carbonetto@gmail.com>
2024-12-28 13:59:37 -0600

    Some fixes to the prepare_pancreas_endocrine_data.py script.

--- scripts/prepare_pancreas_endocrine_data.py
+++ scripts/prepare_pancreas_endocrine_data.py
@@ -1,52 +1,63 @@
+# NOTES:
+#
+# Point to the original source code on GitHub. 
+#
+# Downloaded and extracted files from GSE132188_RAW.tar at
+# https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE132188
+#
+# To run:
+# >>> exec(open('prepare_pancreas_endocrine_data.py').read())
+#
 import numpy as np
-import scanpy.api as sc
+import scipy as sci
+import scanpy as sc
+from anndata import AnnData
+import anndata as ad
 # import matplotlib.pyplot as pl
 # import pandas as pd
 # from scanpy.tools import rna_velocity
-# from anndata import AnnData
 # import seaborn as sns
 # from scipy.sparse import csr_matrix
 # import networkx as nx
 # import xlsxwriter
 # from matplotlib import rcParams
 # import seaborn as sns
-# import scipy as sci
 # import gseapy as gp
 # sc.settings.verbosity = 3
 # sc.logging.print_versions()
 
-# Read cellranger files for all four samples
-filename = './E12_5_counts/mm10/matrix.mtx'
-filename_genes = './E12_5_counts/mm10/genes.tsv'
-filename_barcodes = './E12_5_counts/mm10/barcodes.tsv'
+# Read cellranger files for all four samples.
+filename = '../data/GSE132188_RAW/E12_5_counts/mm10/matrix.mtx'
+filename_genes = '../data/GSE132188_RAW/E12_5_counts/mm10/genes.tsv'
+filename_barcodes = '../data/GSE132188_RAW/E12_5_counts/mm10/barcodes.tsv'
 
 e125 = sc.read(filename).transpose()
 e125.var_names = np.genfromtxt(filename_genes, dtype=str)[:, 1]
-e125.obs_names = np.genfromtxt(filename_barcodes, dtype=str)
+e125.obs_names = 'e125-' + np.genfromtxt(filename_barcodes, dtype=str)
 
-filename = './E13_5_counts/mm10/matrix.mtx'
-filename_genes = './E13_5_counts/mm10/genes.tsv'
-filename_barcodes = './E13_5_counts/mm10/barcodes.tsv'
+filename = '../data/GSE132188_RAW/E13_5_counts/mm10/matrix.mtx'
+filename_genes = '../data/GSE132188_RAW/E13_5_counts/mm10/genes.tsv'
+filename_barcodes = '../data/GSE132188_RAW/E13_5_counts/mm10/barcodes.tsv'
 
 e135 = sc.read(filename).transpose()
 e135.var_names = np.genfromtxt(filename_genes, dtype=str)[:, 1]
-e135.obs_names = np.genfromtxt(filename_barcodes, dtype=str)
+e135.obs_names = 'e135-' + np.genfromtxt(filename_barcodes, dtype=str)
 
-filename = './E14_5_counts/mm10/matrix.mtx'
-filename_genes = './E14_5_counts/mm10/genes.tsv'
-filename_barcodes = './E14_5_counts/mm10/barcodes.tsv'
+filename = '../data/GSE132188_RAW/E14_5_counts/mm10/matrix.mtx'
+filename_genes = '../data/GSE132188_RAW/E14_5_counts/mm10/genes.tsv'
+filename_barcodes = '../data/GSE132188_RAW/E14_5_counts/mm10/barcodes.tsv'
 
 e145 = sc.read(filename).transpose()
 e145.var_names = np.genfromtxt(filename_genes, dtype=str)[:, 1]
-e145.obs_names = np.genfromtxt(filename_barcodes, dtype=str)
+e145.obs_names = 'e145-' + np.genfromtxt(filename_barcodes, dtype=str)
 
-filename = './E15_5_counts/mm10/matrix.mtx'
-filename_genes = './E15_5_counts/mm10/genes.tsv'
-filename_barcodes = './E15_5_counts/mm10/barcodes.tsv'
+filename = '../data/GSE132188_RAW/E15_5_counts/mm10/matrix.mtx'
+filename_genes = '../data/GSE132188_RAW/E15_5_counts/mm10/genes.tsv'
+filename_barcodes = '../data/GSE132188_RAW/E15_5_counts/mm10/barcodes.tsv'
 
 e155 = sc.read(filename).transpose()
 e155.var_names = np.genfromtxt(filename_genes, dtype=str)[:, 1]
-e155.obs_names = np.genfromtxt(filename_barcodes, dtype=str)
+e155.obs_names = 'e155-' + np.genfromtxt(filename_barcodes, dtype=str)
 
 # Add dev. timepoint label for each sample
 e125.obs['day'] = '12.5'
@@ -54,8 +65,16 @@ e135.obs['day'] = '13.5'
 e145.obs['day'] = '14.5'
 e155.obs['day'] = '15.5'
 
-# Create Concatenated anndata object for all timepoints
-alldays = e125.concatenate(e135, e145, e155)
+# Create Concatenated anndata object for all timepoints.
+e125.obs_names_make_unique()
+e135.obs_names_make_unique()
+e145.obs_names_make_unique()
+e155.obs_names_make_unique()
+e125.var_names_make_unique()
+e135.var_names_make_unique()
+e145.var_names_make_unique()
+e155.var_names_make_unique()
+alldays = ad.concat([e125, e135, e145, e155])
 
 # Deleting individual day arrays
 del e125
