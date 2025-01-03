3e52145d57a21b990c9008466298dcb35c9fa038
Peter Carbonetto <peter.carbonetto@gmail.com>
2025-01-03 10:40:32 -0600

    Started new script fit_pancreas_celseq2_gbcd.R.

---
+++ scripts/fit_pancreas_celseq2_gbcd.R
@@ -0,0 +1,41 @@
+# remotes::install_github("stephenslab/gbcd@form-YYT-option")
+# packageVersion("gbcd")
+# 
+library(Matrix)
+library(ebnm)
+library(flashier)
+library(gbcd)
+load("../data/pancreas.RData")
+set.seed(1)
+
+# All the matrix factorizations will have this many topics or factors.
+k <- 9
+
+# Select the CEL-seq2 data (Muraro et al, 2016).
+# This should select 2,285 cells.
+i           <- which(sample_info$tech == "celseq2")
+sample_info <- sample_info[i,]
+counts      <- counts[i,]
+
+# Remove genes that are expressed in fewer than 10 cells.
+x      <- colSums(counts > 0)
+j      <- which(x > 9)
+counts <- counts[,j]
+
+# Compute the shifted log counts.
+a <- 1
+s <- rowSums(counts)
+s <- s/mean(s)
+Y <- MatrixExtra::mapSparse(counts/(a*s),log1p)
+
+# Set a lower bound on the variances.
+n  <- nrow(counts)
+x  <- rpois(1e7,1/n)
+s1 <- sd(log(x + 1))
+
+# Fit a semi-NMF using GBCD.
+#
+# TO DO: Switch to using point-exponential prior.
+#
+fl_gbcd <- fit_gbcd(Y, Kmax = 9, form_YYT = TRUE, maxiter1 = 100,
+                    maxiter2 = 100, maxiter3 = 100, verbose = 3)
