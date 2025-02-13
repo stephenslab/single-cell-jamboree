496509b0046f312fafad8ae0804c64d71dc69eb9
Peter Carbonetto <peter.carbonetto@gmail.com>
2025-02-13 15:51:20 -0600

    Added Structure plots to annotate_newsgroups analysis.

--- analysis/newsgroups_annotate.R
+++ analysis/newsgroups_annotate.R
@@ -1,13 +1,3 @@
-# Structure plots.
-L <- poisson2multinom(pnmf)$L
-newsgroups_topics <- paste0("k",c(1,3,5:10))
-other_topics      <- paste0("k",c(2,4))
-p1 <- structure_plot(L,grouping = topics,gap = 10,topics = newsgroups_topics) +
-  labs(title = "newsgroups topics")
-p2 <- structure_plot(L,grouping = topics,gap = 10,topics = other_topics) +
-  labs(title = "other topics")
-plot_grid(p1,p2,nrow = 2,ncol = 1)
-
 #
 # TO DO NEXT:
 # - Move this code to newsgroups_annotate.Rmd.
