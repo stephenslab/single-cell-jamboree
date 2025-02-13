f2e1acd871809eae4826e93f5c23069a207ad344
Peter Carbonetto <peter.carbonetto@gmail.com>
2025-02-13 15:37:17 -0600

    Added some code chunks to the annotate_newsgroups analysis giving an overview of the newsgroups data.

--- analysis/newsgroups_annotate.R
+++ analysis/newsgroups_annotate.R
@@ -1,18 +1,3 @@
-library(Matrix)
-library(fastTopics)
-library(ggplot2)
-library(cowplot)
-library(ggrepel)
-source("../code/annotation_plots.R")
-load("../data/newsgroups.RData")
-load("../output/newsgroups_topics.RData")
-set.seed(1)
-
-# Remove words that appear in fewer than 10 documents.
-x <- colSums(counts > 0)
-j <- which(x > 9)
-counts <- counts[,j]
-
 # Structure plots.
 L <- poisson2multinom(pnmf)$L
 newsgroups_topics <- paste0("k",c(1,3,5:10))
@@ -41,13 +26,3 @@ p7 <- volcano_plot(de_le,k = "k7",ymax = 30)
 p8 <- volcano_plot(de_le,k = "k8",ymax = 60)
 p9 <- volcano_plot(de_le,k = "k9",ymax = 40)
 p10 <- volcano_plot(de_le,k = "k10",ymax = 40)
-
-# TO DO NEXT, PART 1a: Create plotting functions for visualizing and/or
-# annotating the semi-NMF factors (fl_snmf_ldf). This includes "effect
-# plots".
-
-#
-# PART 1b: Create plotting functions for visualizing and/or annotating
-# the flashier NMF factors (fl_nmf_ldf). This includes "driving gene
-# scatterplots" and "effect plots". (Although I should replace "gene"
-# with something else more general, e.g., feature.)
