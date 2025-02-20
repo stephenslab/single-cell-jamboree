105293f8f21a4fa50160bc5f09a98a67b30c9dd7
Peter Carbonetto <peter.carbonetto@gmail.com>
2025-02-20 12:17:16 -0600

    Removed newsgroups_annotate.R.

--- analysis/newsgroups_annotate.R
+++
@@ -1,6 +0,0 @@
-#
-# TO DO NEXT:
-# - Generate the "effects matrices" (F_le, F_vsnull), that we will analyze.
-# - Create "distinctive features" scatterplots.
-# - Design the "effects plot" on paper first (no R code).
-#
