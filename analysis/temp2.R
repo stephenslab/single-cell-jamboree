773b4e42da1042b85d963ebe57f0af992f43b5a3
Peter Carbonetto <peter.carbonetto@gmail.com>
2025-06-05 09:46:25 -0500

    wflow_publish("pancreas_cytokine.Rmd")

--- analysis/temp2.R
+++
@@ -1,7 +0,0 @@
-k <- 14
-tm <- fit_poisson_nmf(counts,k = k,init.method = "random",method = "em",
-                      numiter = 20,verbose = "none",
-                      control = list(numiter = 4,nc = 8,extrapolate = FALSE))
-tm <- fit_poisson_nmf(counts,fit0 = tm,method = "scd",numiter = 40,
-                      control = list(numiter = 4,nc = 8,extrapolate = TRUE),
-					  verbose = "none")
