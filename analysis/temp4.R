0cb8ea79df6ccb8d523dc1c175a7c1a22356ce05
Peter Carbonetto <peter.carbonetto@gmail.com>
2025-06-27 15:04:00 -0500

    Added scatterplots to compare the gene expression estimates (F) from the topic model.

--- analysis/temp4.R
+++ analysis/temp4.R
@@ -1,28 +1,5 @@
-library(ggrepel)
-
-# Some additional code to possibly incorporate into mcf7.Rmd.
-F <- poisson2multinom(tm)$F
-pdat <- cbind(genes[c("GeneID","Symbol")],log10(F + 1e-6))
-p1 <- ggplot(pdat,aes(x = k3,y = k2)) +
-  geom_point() +
-  geom_abline(intercept = 0,slope = 1,color = "magenta",
-              linetype = "dotted") +
-  theme_cowplot(font_size = 12)
-p2 <- ggplot(pdat,aes(x = k3,y = k1)) +
-  geom_point() +
-  geom_abline(intercept = 0,slope = 1,color = "magenta",
-              linetype = "dotted") +
-  theme_cowplot(font_size = 12)
-p3 <- ggplot(pdat,aes(x = k2,y = k1)) +
-  geom_point() +
-  geom_abline(intercept = 0,slope = 1,color = "magenta",
-              linetype = "dotted") +
-  theme_cowplot(font_size = 12)
-print(plot_grid(p1,p2,p3,nrow = 1,ncol = 3))
-
-# TO DO NEXT: Run de_analysis to obtain the "least extreme" LFCs and
-# the "vsnull" LFCs, and recreate the above scatterplots using the
-# "vsnull" LFCs.
+# TO DO: Figure out way to scale the F matrix so that they are
+# comparable.
 
 F <- ldf(fl_nmf,type = "i")$F
 colnames(F) <- paste0("k",1:4)
