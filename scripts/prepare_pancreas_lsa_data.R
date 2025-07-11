9efc457c4c6fdde418038bb54e645cf069f2099c
Peter Carbonetto <peter.carbonetto@gmail.com>
2025-07-11 10:15:45 -0500

    Started a new script prepare_pancreas_lsa_data.R.

--- scripts/prepare_pancreas_lsa_data.R
+++ scripts/prepare_pancreas_lsa_data.R
@@ -1,22 +1,20 @@
-## ----knitr-opts, include=FALSE------------------------------------------------
-knitr::opts_chunk$set(comment = "#",collapse = TRUE,results = "hold",
-                      fig.align = "center",dpi = 120)
-
-
-## ----load-pkgs, message=FALSE-------------------------------------------------
+# Script to prepare the pancreas LSA data (Stancill et al, 2021) for
+# analysis with NMF methods (including the topic model). See
+# pancreas_cytokine_lsa_clustering.Rmd for background on these data.
+#
+# These data preparation steps are slightly different than what is in
+# pancreas_cytokine_lsa_clustering.Rmd, and therefore I save the final
+# output as "pancreas_cytokine_lsa_v2.RData".
+#
 library(Matrix)
 library(tools)
 library(readr)
-library(fastTopics)
 library(dplyr)
-library(Seurat)
 
-
-## ----set-seed-----------------------------------------------------------------
+# Set the seed for reproducibility.
 set.seed(1)
 
-
-## ----import-barcodes, message=FALSE-------------------------------------------
+# Import the sample information.
 barcodes1<-read_tsv("../data/GSE156175_RAW/GSM5842388_Rep1_S1_barcodes.tsv.gz",
                     col_names = "barcode")
 barcodes2 <- read_tsv("../data/GSE156175_RAW/GSM4726017_S2_barcodes.tsv.gz",
@@ -37,8 +35,7 @@ barcodes <- rbind(barcodes1, barcodes2, barcodes3, barcodes4)
 barcodes$cell_bc <- paste0(barcodes$barcode, "_", barcodes$condition)
 barcodes$condition <- factor(barcodes$condition)
 
-
-## ----import-gene-info, message=FALSE------------------------------------------
+# Import the gene information.
 genes1 <- read_tsv("../data/GSE156175_RAW/GSM5842388_Rep1_S1_features.tsv.gz",
                    col_names = c("ensembl", "symbol", "type"))
 genes2 <- read_tsv("../data/GSE156175_RAW/GSM4726017_S2_features.tsv.gz",
@@ -50,8 +47,7 @@ genes4 <- read_tsv("../data/GSE156175_RAW/GSM4726019_S4_features.tsv.gz",
 genes <- genes1
 genes$type <- factor(genes$type)
 
-
-## ----import-counts, message=FALSE---------------------------------------------
+# Now import in the read counts.
 counts1 <- t(readMM("../data/GSE156175_RAW/GSM5842388_Rep1_S1_matrix.mtx.gz"))
 counts2 <- t(readMM("../data/GSE156175_RAW/GSM4726017_S2_matrix.mtx.gz"))
 counts3 <- t(readMM("../data/GSE156175_RAW/GSM4726018_S3_matrix.mtx.gz"))
@@ -72,15 +68,21 @@ counts <- rbind(
 )
 counts <- as(counts, "CsparseMatrix")
 
+# Clean up the working environment.
+rm(counts1,counts2,counts3,counts4)
+rm(genes1,genes2,genes3,genes4)
+rm(barcodes1,barcodes2,barcodes3,barcodes4)
 
-## ----filter-genes-1-----------------------------------------------------------
+# Remove genes that are not expressed in any cell.
 x      <- colSums(counts)
 j      <- which(x > 0)
 genes  <- genes[j,]
 counts <- counts[,j]
 
+stop()
 
-## ----filter-cells-------------------------------------------------------------
+# Remove cells in terms (i) total UMI count and (ii) very few
+# expressed genes.
 x        <- rowSums(counts > 0)
 i        <- which(x > 2000)
 barcodes <- barcodes[i,]
@@ -89,7 +91,6 @@ x        <- rowSums(counts)
 i        <- which(x <= 60000)
 barcodes <- barcodes[i,]
 counts   <- counts[i,]
-
 
 ## ----filter-cells-2-----------------------------------------------------------
 mito_genes <- which(substr(genes$symbol,1,2) == "mt")
@@ -100,7 +101,6 @@ i          <- which(prop_mito < 0.1)
 barcodes   <- barcodes[i,]
 counts     <- counts[i,]
 
-
 ## ----filter-genes-2-----------------------------------------------------------
 j <- which(!(grepl("^mt-",genes$symbol) | grepl("^Rp[sl]",genes$symbol)))
 genes  <- genes[j,]
@@ -109,73 +109,14 @@ j      <- which(genes$symbol != "Malat1")
 genes  <- genes[j,]
 counts <- counts[,j]
 
-
 ## ----filter-genes-3-----------------------------------------------------------
 x      <- colSums(counts > 0)
 j      <- which(x > 2)
 genes  <- genes[j,]
 counts <- counts[,j]
 
-
 ## ----write-outputs, eval=FALSE------------------------------------------------
 # save(list = c("barcodes","genes","counts"),
 #      file = "pancreas_cytokine_lsa_v2.RData")
 # resaveRdaFiles("pancreas_cytokine_lsa_v2.RData")
-
-
-## ----fit-topic-model, cache=TRUE----------------------------------------------
-set.seed(1)
-tm_fit <- fit_poisson_nmf(X = counts,k = 12,control = list(nc = 7),
-                          verbose = "none")
-
-
-## ----structure-plot, fig.height=1.75, fig.width=7, results="hide"-------------
-set.seed(1)
-structure_plot(tm_fit,n = Inf)
-
-
-## ----annotation-heatmap, fig.height=2.5, fig.width=4--------------------------
-scale_rows <- function (A)
-  A / apply(A,1,max)
-marker_genes <- c("Ins1","Ins2","Gcg","Sst","Ppy","Krt17","Ccr5",
-                  "Col1a1","Cd34","Cpa1")
-F <- poisson2multinom(tm_fit)$F
-F <- F[marker_genes,]
-F <- scale_rows(F)
-rownames(F) <- marker_genes
-annotation_heatmap(F,select_features = "all",verbose = FALSE)
-
-
-## ----clustering, fig.height=2.5, fig.width=7, results="hide"------------------
-L <- poisson2multinom(tm_fit)$L
-L_df <- as.data.frame(L)
-L_df <- L_df %>%
-  mutate(
-    cluster = case_when(
-      k2 > (1/3) ~ "Acinar",
-      k3 > (1/3) ~ "Endothelial/Mesnchymal",
-      k5 > (1/3) ~ "Ductal",
-      k8 > (1/3) ~ "Delta",
-      k9 > (1/3) ~ "Alpha",
-      k10 > (1/3) ~ "Gamma",
-      k11 > (1/3) ~ "Macrophage",
-      TRUE ~ "Beta"
-    )
-  )
-structure_plot(tm_fit, grouping = L_df$cluster, gap = 25, n = Inf)
-
-
-## ----elbo-plot, fig.height=3, fig.width=3-------------------------------------
-barcodes$celltype <- L_df$cluster
-counts <- counts[,!duplicated(colnames(counts))]
-so <- CreateSeuratObject(counts = Matrix::t(counts), meta.data = barcodes)
-so <- SCTransform(so, verbose = FALSE)
-so <- RunPCA(so, verbose = FALSE)
-ElbowPlot(so)
-
-
-## ----umap-plot, fig.height=3, fig.width=5-------------------------------------
-so <- RunUMAP(so, dims = 1:10, verbose = FALSE)
-DimPlot(so, group.by = "celltype") +
-  theme_cowplot(font_size = 10)
 
