da5977901604064a01159dbc76c55551fad043d9
Peter Carbonetto <peter.carbonetto@gmail.com>
2025-07-17 15:49:40 -0500

    Switched to using flashier_nmf() from the singlecelljamboreeR package in the pancreas_cytokine_S1_factors analysis.

--- analysis/temp4.R
+++
@@ -1,8 +0,0 @@
-set.seed(1)
-n  <- nrow(shifted_log_counts)
-x  <- rpois(1e7,1/n)
-s1 <- sd(log(x + 1))
-set.seed(1)
-fl_nmf_new <- 
-  singlecelljamboreeR::flashier_nmf(shifted_log_counts,k = 13,n.threads = 8,
-                                    var_type = 2,S = s1)
