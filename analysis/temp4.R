0a194acae3e2f2067e0fafb6253caa938efefde5
Peter Carbonetto <peter.carbonetto@gmail.com>
2025-08-12 16:49:05 -0500

    Moved some of the code from temp4.R into lps.Rmd.

--- analysis/temp4.R
+++ analysis/temp4.R
@@ -1,14 +1,5 @@
 # Some code to be added to the lps.Rmd workflowr analysis.
 
-# --- flashier-nmf-k9 ---
-set.seed(1)
-n  <- nrow(shifted_log_counts)
-x  <- rpois(1e7,1/n)
-s1 <- sd(log(x + 1))
-set.seed(1)
-fl_nmf_k9 <- flashier_nmf(shifted_log_counts,k = 9,greedy_init = TRUE,
-                          var_type = 2,S = s1)
-
 # --- structure-plot-flashier-nmf-k9 ---
 rows <- order(samples$timepoint)
 topic_colors <- c("powderblue","dodgerblue","olivedrab","limegreen",
@@ -21,12 +12,13 @@ structure_plot(L,grouping = samples$tissue,gap = 4,
                loadings_order = rows) +
   labs(fill = "",y = "membership")
 
-# --- flashier-nmf-k15 ---
-set.seed(1)
-fl_nmf <- flashier_nmf(shifted_log_counts,k = 15,greedy_init = TRUE,
-                       var_type = 2,S = s1)
-
 # --- structure-plot-flashier-nmf ---
-#
-# TO DO
-#
+L <- ldf(fl_nmf,type = "i")$L
+p <- structure_plot(L,grouping = samples$tissue,gap = 4,
+               topics = 1:15,colors = topic_colors,
+			   loadings_order = rows) +
+  labs(fill = "",y = "membership") +
+  theme(axis.text.x = element_text(angle = 0,hjust = 0.5),
+        legend.key.height = unit(0.01,"cm"),
+        legend.key.width = unit(0.25,"cm"),
+        legend.text = element_text(size = 7))
