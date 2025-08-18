799b0c003663faedf379ad1cf74bc3920529a370
Peter Carbonetto <peter.carbonetto@gmail.com>
2025-08-18 10:08:39 -0500

    Added initial fastTopics and flashier_nmf calls to the compute_yeast_factors.R script.

--- scripts/compute_yeast_factors.R
+++ scripts/compute_yeast_factors.R
@@ -1,21 +1,57 @@
 # Analyze the pancreas data using flashier and fastTopics.
 #
-# sinteractive --mem=16G -c 8 --time=24:00:00
+# sinteractive --mem=16G -c 8 --time=24:00:00 -p mstephens \
+#   --account=pi-mstephens
 # module load R/4.2.0
 # .libPaths()[1]
 # /home/pcarbo/R_libs_4_20
+#
 library(tools)
 library(Matrix)
-library(fastTopics)
-library(flashier)
+library(fastTopics) # 0.7.25
+library(flashier) # 1.0.56
+library(singlecelljamboreeR) # 0.1.41
 load("../data/yeast.RData")
 set.seed(1)
 
-# (1) Fit a Poisson NMF using fastTopics.
-# TO DO.
+# Set up the "timings" data structure.
+timings <- list(tm = 0,fl_nmf = 0)
 
-# (2) Fit an NMF using flashier.
+# Compute the shifted log counts.
+a <- 1
+s <- rowSums(counts)
+s <- s/mean(s)
+shifted_log_counts <- MatrixExtra::mapSparse(counts/(a*s),log1p)
+
+# Set a lower bound on the variances.
+n  <- nrow(counts)
+x  <- rpois(1e7,1/n)
+s1 <- sd(log(x + 1))
+
+# Fit a Poisson NMF using fastTopics.
+t0 <- proc.time()
+tm0 <- fit_poisson_nmf(counts,k = 15,numiter = 20,method = "em",
+                      control = list(numiter = 4,nc = 8,extrapolate = FALSE),
+                      init.method = "random",verbose = "detailed")
+tm <- fit_poisson_nmf(counts,fit0 = tm0,numiter = 20,method = "scd",
+                      control = list(numiter = 4,nc = 8,extrapolate = TRUE),
+                      verbose = "detailed")
+t1 <- proc.time()
+timings$tm <- t1 - t0
+
+# Fit an NMF using flashier.
+t0 <- proc.time()
+fl_nmf <- flashier_nmf(shifted_log_counts,k = 15,greedy_init = TRUE,
+                       var_type = 2,S = s1,verbose = 2,maxiter = 20)
+t1 <- proc.time()
+timings$fl_nmf <- t1 - t0
+
+# Fit an NMF with "cross-cutting" factors.
 # TO DO.
 
 # Save the model fits to an .Rdata file.
-# TO DO.
+session_info <- sessionInfo()
+save(list = c("tm","timings","session_info"),
+     file = "yeast_factors.RData")
+# resaveRdaFiles("yeast_factors.RData")
+
