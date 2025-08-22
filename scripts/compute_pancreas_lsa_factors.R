07ad439035decad35c3617a66ca7838a4194f6b8
Peter Carbonetto <peter.carbonetto@gmail.com>
2025-08-22 16:35:27 -0500

    wflow_publish("pancreas_lsa_factors.Rmd", view = FALSE)

---
+++ scripts/compute_pancreas_lsa_factors.R
@@ -0,0 +1,30 @@
+# Analyze the pancreas LSA data using flashier and fastTopics.
+# See prepare_pancreas_lsa_data.R for background on these data.
+#
+# sinteractive --mem=16G -c 8 --time=12:00:00 -p mstephens \
+#   --account=pi-mstephens
+# module load R/4.2.0
+# .libPaths()[1]
+# /home/pcarbo/R_libs_4_20
+#
+library(tools)
+library(Matrix)
+library(fastTopics)          # 0.7.25
+library(flashier)            # 1.0.56
+library(singlecelljamboreeR) # 0.1.41
+load("../data/pancreas_cytokine_lsa_v2.RData")
+set.seed(1)
+
+# Set up the "timings" data structure.
+timings <- list(tm = 0,fl_nmf = 0)
+
+# Compute the shifted log counts.
+a <- 1
+s <- rowSums(counts)
+s <- s/mean(s)
+shifted_log_counts <- MatrixExtra::mapSparse(counts/(a*s),log1p)
+
+# Set a lower bound on the variances.
+n  <- nrow(counts)
+x  <- rpois(1e7,1/n)
+s1 <- sd(log(x + 1))
