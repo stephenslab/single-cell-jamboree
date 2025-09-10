7370ec3b85f7356af8e9bf6831862fb0f19f9b4f
Peter Carbonetto <peter.carbonetto@gmail.com>
2025-09-10 15:32:26 -0500

    Working on new script prepare_pancreas_mstz_data.R.

---
+++ scripts/prepare_pancreas_mstz_data.R
@@ -0,0 +1,17 @@
+# Downloaded and extracted the following tar archives from GEO,
+# accession GSE128565:
+#
+# GSM3680207_G1_counts.tar.gz
+# ...
+# 
+# GSM3680207  No STZ
+# GSM3680208  STZ
+# GSM3680209  STZ insulin
+# GSM3680210  STZ GLP-1
+# GSM3680211  STZ estrogen
+# GSM3680212  STZ GLP-1/estrogen
+# GSM3680213  STZ GLP-1/estrogen + insulin
+# 
+library(Matrix)
+library(tools)
+# TO DO.
