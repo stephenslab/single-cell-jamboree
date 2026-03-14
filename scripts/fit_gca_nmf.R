6663d936f9157940d0180ab89dce253f8fad32ed
Peter Carbonetto <peter.carbonetto@gmail.com>
2026-03-13 23:38:31 -0500

    I have a first draft of the script fit_gca_nmf.R for fitting an nmf model to the combined gca data.

--- scripts/fit_gca_nmf.R
+++ scripts/fit_gca_nmf.R
@@ -14,12 +14,12 @@ library(singlecelljamboreeR) # 0.1.41
 load("../data/gca.RData")
 set.seed(1)
 k <- 20
-
-stop()
+outfile <- sprintf("gca_nmf_k=%d.RData",k)
+cat("k =",k,"\n")
+cat("outfile =",outfile,"\n")
 
 # Filter out genes that are expressed in fewer than 10 cells.
-j      <- which(colSums(counts > 0) > 9)
-genes  <- genes[j,]
+j <- which(colSums(counts > 0) > 9)
 counts <- counts[,j]
 
 # Compute the shifted log counts.
@@ -33,41 +33,20 @@ n  <- nrow(counts)
 x  <- rpois(1e7,1/n)
 s1 <- sd(log(x + 1))
 
-# Fit a Poisson NMF using fastTopics.
-# This step is expected to take about 2 h. 
-t0 <- proc.time()
-tm0 <- fit_poisson_nmf(counts,k = k,numiter = 200,method = "em",
-                      control = list(numiter = 4,nc = 8,extrapolate = FALSE),
-                      init.method = "random",verbose = "detailed")
-tm <- fit_poisson_nmf(counts,fit0 = tm0,numiter = 200,method = "scd",
-                      control = list(numiter = 4,nc = 8,extrapolate = TRUE),
-                      verbose = "detailed")
-t1 <- proc.time()
-print(t1 - t0)
-timings$tm <- t1 - t0
-
 # Fit an NMF using flashier.
-# This step is expected to take about 3 h.
+# This step is expected to take about X h.
+#
+# TO DO: Change to maxiter = 200.
+#
 t0 <- proc.time()
 fl_nmf <- flashier_nmf(shifted_log_counts,k = k,greedy_init = TRUE,
-                       var_type = 2,S = s1,verbose = 2,maxiter = 200)
+                       var_type = 2,S = s1,verbose = 2,maxiter = 20)
 t1 <- proc.time()
 print(t1 - t0)
-timings$fl_nmf <- t1 - t0
 
-# Fit an NMF with "cross-context" factors.
-cc_factors <- c(5,12)
-fl_nmf_cc <- convert_factors_nn_to_pn(fl_nmf,cc_factors,shifted_log_counts,
-                                      S = s1,var_type = 2)
-fl_nmf_cc <- flash_backfit(fl_nmf_cc,extrapolate = FALSE,maxiter = 100,
-                           verbose = 2)
-fl_nmf_cc <- flash_backfit(fl_nmf_cc,extrapolate = TRUE,maxiter = 100,
-                           verbose = 2)
-
-# Save the model fits to an .Rdata file.
+# Save the model fit to an .Rdata file.
 session_info <- sessionInfo()
 fl_nmf_ldf <- ldf(fl_nmf,type = "i")
-fl_nmf_cc_ldf <- ldf(fl_nmf_cc,type = "i")
-save(list = c("tm","fl_nmf_ldf","fl_nmf_cc_ldf","timings","session_info"),
-     file = "pancreas_lsa_factors.RData")
-resaveRdaFiles("pancreas_lsa_factors.RData")
+save(list = c("fl_nmf_ldf","session_info"),
+     file = outfile)
+resaveRdaFiles(outfile)
