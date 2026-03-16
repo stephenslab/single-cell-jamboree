cab06660688c8f9d0ee2e2c08676339cfae59b34
Peter Carbonetto <peter.carbonetto@gmail.com>
2026-03-16 00:08:03 -0500

    Small edit to gca.R.

--- analysis/gca.R
+++ analysis/gca.R
@@ -28,14 +28,14 @@ L <- fl_nmf_ldf$L
 k <- ncol(L)
 colnames(L) <- paste0("k",1:k)
 pdat <- cbind(sample_info,L)
-p1 <- ggplot(subset(pdat,k30 > 0.01),aes(x = k30)) +
+p1 <- ggplot(subset(pdat,k3 > 0.01),aes(x = k3)) +
   geom_histogram(fill = "black",color = "white",bins = 24) +
   facet_grid(rows = vars(origin),scales = "free_y") +
   theme_cowplot(font_size = 12)
-p2 <- ggplot(subset(pdat,k30 > 0.01),aes(x = k30)) +
+p2 <- ggplot(subset(pdat,k3 > 0.01),aes(x = k3)) +
   geom_histogram(fill = "black",color = "white",bins = 24) +
   facet_wrap(facets = vars(celltype),scales = "free_y") +
   theme_cowplot(font_size = 8)
 with(sample_info,table(celltype,origin))
 # Notes:
-# - Nice examples: k = 2, 3, 4, 5, 7, 9, 22, 25 
+# - Nice examples: k = 2, *3*, 4, 5, 7, 9, 22, 25 
