9814258d952794246b5f5b5cd6a895f712b6ada8
Peter Carbonetto <peter.carbonetto@gmail.com>
2026-05-30 21:25:52 -0500

    A few adjustments to the compute_gtex_factors.R script.

--- scripts/compute_gtex_factors.R
+++ scripts/compute_gtex_factors.R
@@ -1,8 +1,8 @@
 # gtex_v11.RData can be downloaded from this shared Box folder: 
 # https://uchicago.box.com/s/l0emrjcqpw5yat1zaygtr1v3akq7de1t
 #
-# sinteractive -p mstephens --mem=16G -c 20 --nodelist=midway2-0440 \
-#   --time=24:00:00
+# sinteractive -p mstephens --mem=24G -c 20 --nodelist=midway2-0440 \
+#   --time=48:00:00
 # module load R/4.2.0
 # .libPaths()[1]
 # /home/pcarbo/R_libs_4_20
@@ -19,10 +19,10 @@ set.seed(1)
 # Fit a Poisson NMF model using fastTopics.
 # This step is expected to take X h.
 t0 <- proc.time()
-tm <- fit_poisson_nmf(counts,k = k,numiter = 20,method = "em",
+tm <- fit_poisson_nmf(counts,k = k,numiter = 100,method = "em",
                       control = list(numiter = 4,nc = 20,extrapolate = FALSE),
                       init.method = "random",verbose = "detailed")
-tm <- fit_poisson_nmf(counts,fit0 = tm,numiter = 20,method = "scd",
+tm <- fit_poisson_nmf(counts,fit0 = tm,numiter = 100,method = "scd",
                       control = list(numiter = 4,nc = 20,extrapolate = TRUE),
                       verbose = "detailed")
 t1 <- proc.time()
